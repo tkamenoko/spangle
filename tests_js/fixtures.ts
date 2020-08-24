632b66e25e6bc0a6b53d530000ea527187cc7f91
T. Kameyama <tkamenoko@vivaldi.net>
2020-08-24 10:28:28 +0900

    rm: js related codes

--- tests_js/fixtures.ts
+++
@@ -1,7 +0,0 @@
-import { spawn } from 'child_process';
-
-
-export const runServer = ({ app = "app:api", port = 3000 } = {}) => {
-    const importStr = "tests_js.src." + app
-    return spawn("poetry", ["run", "hypercorn", "-b", `127.0.0.1:${port}`, importStr], { detached: true })
-}
